d41ebb3b4ba7d5a1d239676b07ddca0fea6b8d4e
MasaMesgho <james_sam_whittingham@outlook.com>
2025-10-18 21:42:18 +0100

    cardlogic
    Adding Card Logic

--- Assets/PlayerCard.cs
+++ Assets/PlayerCard.cs
@@ -5,19 +5,54 @@ using UnityEngine.U2D;
 
 public class PlayerCard : MonoBehaviour
 {
+    string[] card;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         var deck = GameObject.Find("Player Deck");
-        string[] card = deck.GetComponent<playerDeck>().Draw();
+        this.card = deck.GetComponent<playerDeck>().Draw();
         deck = default;
-        Sprite cardFace = Resources.Load<Sprite>("Cards\\Fantasy_Cards_0");
-        this.gameObject.GetComponent<SpriteRenderer>().sprite = cardFace;
+        Sprite[] cardFaces = Resources.LoadAll<Sprite>("FantasyCards");
+        int num;
+        if (card[0] == "A") { num = 0; }
+        else
+        {
+            num = int.Parse(card[0]);
+        }
+        switch (card[1])
+        {
+            case "Clubs":
+                break;
+            case "Hearts":
+                num += 13;
+                break;
+            case "Diamond":
+                num += 26;
+                break;
+            case "Spades":
+                num += 39;
+                break;
+
+        }
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = cardFaces[num];
     }
 
-    // Update is called once per frame
+        // Update is called once per frame
     void Update()
     {
         
     }
+
+    public void Discard()
+    {
+        var discard = GameObject.Find("Player Discard");
+        discard.GetComponent<playerDiscard>().AddCard(this.card);
+        this.enabled = false;
+    }
+    public void Draw()
+    {
+        var deck = GameObject.Find("Player Deck");
+        this.card = deck.GetComponent<playerDeck>().Draw();
+    }
+
 }
--- Assets/playerDiscard.cs
+++ Assets/playerDiscard.cs
@@ -1,11 +1,14 @@
 using NUnit.Framework;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
 using UnityEngine;
 
 public class playerDiscard : MonoBehaviour
 {
 
     List<string[]> discardList = new List<string[]>();
+    string[] lastAdded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,9 +22,20 @@ public class playerDiscard : MonoBehaviour
         
     }
 
-    public void Discard(string[] card)
+    public void AddCard(string[] card)
     {
+        discardList.Add(card);
+        lastAdded = card;
+    }
 
+    public List<string[]> GetAllCards()
+    {
+        List<string[]> temp = discardList;
+        discardList.Clear();
+        string[] temp2 = new string[2];
+        temp2.AddRange("None");
+        lastAdded = temp2;
+        return temp;
     }
 
 }
