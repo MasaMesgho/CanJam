fb9e418f049d2b3b31a79c42cb569a0afae8b57c
MasaMesgho <james_sam_whittingham@outlook.com>
2025-10-18 18:30:22 +0100

    deck and discard
    finished deck
    added basic discard

---
+++ Assets/playerDeck.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+using NUnit.Framework.Internal.Builders;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class playerDeck : MonoBehaviour
+{
+    string[] numbers = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+    string[] suits = new string[] { "Clubs", "Hearts", "Spades", "Diamonds" };
+    List<string[]> deckList = new List<string[]>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        foreach (string suit in suits)
+        {
+            foreach (string number in numbers)
+            {
+                string[] temp = new string[] { number, suit };
+                deckList.Add(temp);
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string[] draw()
+    {
+        int rint = Random.Range(0, deckList.Count - 1);
+        string[] temp = deckList[rint];
+        deckList.Remove(temp);
+        return temp;
+    }
+
+}
---
+++ Assets/playerDiscard.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerDiscard : MonoBehaviour
+{
+
+    List<string[]> discardList = new List<string[]>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Discard(string[] card)
+    {
+
+    }
+
+}
