0b7a0c0611cffd1cdedfad17c8e92d265a0c427e
mindlyanarch <29086791@students.lincoln.ac.uk>
2025-10-18 21:46:40 +0100

    merge?

---
+++ Assets/playerDiscard.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class playerDiscard : MonoBehaviour
+{
+
+    List<string[]> discardList = new List<string[]>();
+    string[] lastAdded;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddCard(string[] card)
+    {
+        discardList.Add(card);
+        lastAdded = card;
+    }
+
+    public List<string[]> GetAllCards()
+    {
+        List<string[]> temp = discardList;
+        discardList.Clear();
+        string[] temp2 = new string[2];
+        temp2.AddRange("None");
+        lastAdded = temp2;
+        return temp;
+    }
+
+}
