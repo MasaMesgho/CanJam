f9e1ad192d4cd66259829d0219dfb5cc26b5a3dc
MasaMesgho <james_sam_whittingham@outlook.com>
2025-10-18 22:14:19 +0100

    merge
    full merge

--- Assets/PlayerCard.cs
+++ Assets/PlayerCard.cs
@@ -1,40 +1,45 @@
+using System.Linq;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.U2D;
 
 public class PlayerCard : MonoBehaviour
 {
-    string[] card;
+    public string[] card;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         var deck = GameObject.Find("Player Deck");
         this.card = deck.GetComponent<playerDeck>().Draw();
         deck = default;
-        Sprite[] cardFaces = Resources.LoadAll<Sprite>("FantasyCards");
+        Sprite[] cardFaces1 = Resources.LoadAll<Sprite>("Swords_Ranks");
+        Sprite[] cardFaces2 = Resources.LoadAll<Sprite>("Skulls_Ranks");
+        Sprite[] cardFaces3 = Resources.LoadAll<Sprite>("Shields_Ranks");
+        Sprite[] cardFaces4 = Resources.LoadAll<Sprite>("Magics_Ranks");
         int num;
         if (card[0] == "A") { num = 0; }
         else
         {
-            num = int.Parse(card[0]);
+            num = int.Parse(card[0])-1;
         }
         switch (card[1])
         {
             case "Clubs":
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = cardFaces1[num];
                 break;
             case "Hearts":
-                num += 13;
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = cardFaces2[num];
                 break;
-            case "Diamond":
-                num += 26;
+            case "Diamonds":
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = cardFaces4[num];
                 break;
             case "Spades":
-                num += 39;
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = cardFaces3[num];
                 break;
 
         }
-        this.gameObject.GetComponent<SpriteRenderer>().sprite = cardFaces[num];
     }
 
         // Update is called once per frame
